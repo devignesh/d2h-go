f507e9e1d39bc86f9f78d42c0fff0f69d4732bf6
devignesh <vinesh1865@gmail.com>
2020-09-30 01:58:37 +0530

    adding functionality raws and getting the input option from cli

--- d2h.go
+++ d2h.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 //Declaring user defined fields
@@ -33,12 +34,126 @@ var service = map[string]int{
 	"LearnCooking": 100,
 }
 
+// getting Current Balance
+func (d *Dth) getBalance() {
+
+}
+
+//Adding New Balance
+func (d *Dth) recharge() {
+
+}
+
+//Showing list of services, packs and chanels
+func (d *Dth) listOffers() {
+
+}
+
+//Subscribe new packages
+func (d *Dth) subscribrChannelPacks() {
+
+}
+
+//Adding channels
+func (d *Dth) addChannelToexsistSubscription() {
+
+}
+
+//Subscribe New services
+func (d *Dth) subscribeNewService() {
+
+}
+
+//Listing account Details
+func (d *Dth) getSubscriptionDetails() {
+
+}
+
+//Updating email and phone nuber
+func (d *Dth) updateEmailandPhone() {
+
+}
+
+//Function for option in D2h
+func help() {
+
+	fmt.Println("1: View current balance in the account")
+	fmt.Println("2: Recharge Account")
+	fmt.Println("3: View available packs, channels and services")
+	fmt.Println("4: Subscribe to base packs")
+	fmt.Println("5: Add channels to an existing subscription")
+	fmt.Println("6: Subscribe to special services")
+	fmt.Println("7: View current subscription details")
+	fmt.Println("8: Update email and phone number for notifications")
+	fmt.Println("9: Exit\n")
+	fmt.Println("Enter the option")
+
+}
+
 //Main function
 func main() {
 
 	fmt.Println("Welcome to DishTV\n")
 
 	dt := &Dth{name: "Vignesh", email: "vinesh1865@gmail.com", phone: 9047660920, balance: 100, packs: "Gold", chanels: "Zee, Sony", services: "LearnEnglish"}
-	fmt.Println(dt)
+	// fmt.Println(dt)
+	help()
 
+	for {
+		var option int
+		_, err := fmt.Scanf("%d", &option) //Getting user input for options
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		if option > 9 {
+
+			fmt.Println("Please Enter valid option\n")
+			help()
+
+		} else {
+
+			switch option {
+
+			case 1:
+
+				dt.getBalance()
+
+			case 2:
+
+				dt.recharge()
+
+			case 3:
+
+				dt.listOffers()
+
+			case 4:
+
+				dt.subscribrChannelPacks()
+
+			case 5:
+
+				dt.addChannelToexsistSubscription()
+
+			case 6:
+
+				dt.subscribeNewService()
+
+			case 7:
+
+				dt.getSubscriptionDetails()
+
+			case 8:
+
+				dt.updateEmailandPhone()
+
+			case 9:
+
+				os.Exit(1)
+			}
+			help()
+			continue
+		}
+	}
 }
